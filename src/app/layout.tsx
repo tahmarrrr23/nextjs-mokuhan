adb350af43e0a2bfcced49001ea3b4a816a0a6fe
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-10-13 21:22:31 +0900

    feat: add font and global style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import "@/styles/globals.css";
 import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Next.js Ignition",
@@ -6,13 +8,17 @@ export const metadata: Metadata = {
     "🚀 Next.js - my personal boilerplate for fast, consistent, and modern",
 };
 
+const globalFont = Roboto({
+  subsets: ["latin"],
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={globalFont.className}>
       <body>{children}</body>
     </html>
   );
