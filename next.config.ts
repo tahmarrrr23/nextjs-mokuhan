cd43455d31a034b9ca9b22843ba01aba6e9949f8
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-10-19 03:55:04 +0900

    refactor: rename var

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const config: NextConfig = {
   devIndicators: false,
 };
 
-export default nextConfig;
+export default config;
