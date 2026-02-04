d98c863be40b904b9a5e53e13272d3dac4f9c009
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2026-02-04 23:23:51 +0900

    feat: restructure layout and update metadata format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
 import { globalFont } from "@/libs/font";
 import "@/styles/globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Next.js Ignition",
+  title: {
+    default: "Next.js Ignition",
+    template: "%s | Next.js Ignition",
+  },
   description:
     "My personal Next.js boilerplate for fast, consistent, and modern web apps.",
 };
@@ -13,9 +18,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const environment = process.env.NODE_ENV;
+
   return (
     <html lang="en">
-      <body className={globalFont.className}>{children}</body>
+      <body className={globalFont.className}>
+        <Header environment={environment} />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-import { Footer } from "@/components/footer";
-import { Header } from "@/components/header";
 import { HomePage } from "@/components/home-page";
 
 export default function Home() {
-  const environment = process.env.NODE_ENV;
-
-  return (
-    <>
-      <Header environment={environment} />
-      <HomePage />
-      <Footer />
-    </>
-  );
+  return <HomePage />;
 }
